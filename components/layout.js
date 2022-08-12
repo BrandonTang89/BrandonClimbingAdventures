05d563430945d94154341b195a1eba407a4209e5
BrandonTang89 <tangyuhanbrandon@gmail.com>
2022-08-12 19:38:59 +0800

    added favicon as logo

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+import logoPic from '/public/images/favicon.png';
+
 export default function Layout({ children }) {
   return (
     <div>
@@ -24,13 +27,28 @@ export default function Layout({ children }) {
           href="/images/favicon-16x16.png"
         />
         <link rel="manifest" href="/images/site.webmanifest" />
-        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#5bbad5" />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <div className="bg-amber-700 flex flex-wrap">
+      <div className="bg-amber-700 flex flex-wrap px-10 py-2">
+        <div className="hidden lg:block">
+          <Image
+            src={logoPic}
+            alt="logo"
+            width="60"
+            height="60"
+            className="rounded-lg align-middle"
+          />
+        </div>
         <div className="text-3xl text-zinc-300 mx-4 my-3 font-bold">
-          <Link href="/"> Brandon&apos;s Climbing Adventures</Link>
+          <Link href="/">
+            <div>Brandon&apos;s Climbing Adventures</div>
+          </Link>
         </div>
         <Link href="/about">
           <div className="text-2xl text-zinc-300 px-4 py-3 hover:text-zinc-100 hover:bg-amber-800">
